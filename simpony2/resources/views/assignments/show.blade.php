db33f1965c87d602785ac2cc72542ed5dd04eb58
adhikadhanes <adhika.dhaneswara@gmail.com>
2016-04-15 15:14:24 +0700

    ngubah variable biar ga error

--- simpony2/resources/views/assignments/show.blade.php
+++ simpony2/resources/views/assignments/show.blade.php
@@ -86,77 +86,6 @@
         </table>
     </div>
         
-        <div class="col-md-6">
-          <!-- Box Comment -->
-          <div class="box box-widget">
-            <div class="box-header with-border">
-              <div class="user-block">
-                <!-- <img class="img-circle" src="../dist/img/user1-128x128.jpg" alt="User Image"> -->
-                <span class="username"><a href="#">Assignment</a></span>
-              </div>
-              <!-- /.user-block -->
-              <div class="box-tools">
-                <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="Mark as read">
-                  <i class="fa fa-circle-o"></i></button>
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-              </div>
-              <!-- /.box-tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-            <p>no picture available yet</p>
-
-             
-            </div>
-            <!-- /.box-body -->
-            <div class="box-footer box-comments">
-              <div class="box-comment">
-                <!-- User image -->
-                <!-- <img class="img-circle img-sm" src="../dist/img/user3-128x128.jpg" alt="User Image">
- -->
-                <div class="comment-text">
-                      <span class="username">
-                        Tes
-                        <span class="text-muted pull-right">8:03 PM Today</span>
-                      </span><!-- /.username -->
-                  It is a long established fact that a reader will be distracted
-                  by the readable content of a page when looking at its layout.
-                </div>
-                <!-- /.comment-text -->
-              </div>
-              <!-- /.box-comment -->
-              <div class="box-comment">
-                <!-- User image -->
-                <!-- <img class="img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="User Image"> -->
-
-                <div class="comment-text">
-                      <span class="username">
-                        Tes 2
-                        <span class="text-muted pull-right">8:03 PM Today</span>
-                      </span><!-- /.username -->
-                  It is a long established fact that a reader will be distracted
-                  by the readable content of a page when looking at its layout.
-                </div>
-                <!-- /.comment-text -->
-              </div>
-              <!-- /.box-comment -->
-            </div>
-            <!-- /.box-footer -->
-            <div class="box-footer">
-              <form action="#" method="post">
-                
-                <!-- .img-push is used to add margin to elements next to floating images -->
-                <div class="img-push">
-                  <input type="text" class="form-control input-sm" placeholder="Press enter to post comment">
-                </div>
-              </form>
-            </div>
-            <!-- /.box-footer -->
-          </div>
-          <!-- /.box -->
-        </div>
 
 <head>
  <style>
@@ -393,7 +322,7 @@
 </div>
 </div>
 
-
+<?php $comments = [];?>
 @foreach($comments as $item)
                 {{-- */$x++;/* --}}
 
@@ -403,18 +332,17 @@
                       <span class="username">
                         <strong>{{ $item->Sender }}</strong>
                         <span class="text-muted pull-right">{{ $item->created_at }} </span>
-                      </span><!-- /.username -->
+                      </span>
                 <p>
                       {{$item->Comment}}
                    <p>
                 </div>
-                <!-- /.comment-text -->
+                
               </div>
               <hr>
             <br>
 
 @endforeach
-
 
 
 
